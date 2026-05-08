a9c00e75c6e68901d476140bed2bd7d56a12cf69
TsukubaKobu <tujunchi27@gmail.com>
2026-05-08 14:26:23 +0900

    feat: expose cache and settings APIs via preload bridge

--- preload.js
+++ preload.js
@@ -2,4 +2,9 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
   searchDatabase: (args) => ipcRenderer.invoke('search-database', args),
+  getCache: () => ipcRenderer.invoke('get-cache'),
+  setCache: (key, results) => ipcRenderer.invoke('set-cache', { key, results }),
+  clearCache: () => ipcRenderer.invoke('clear-cache'),
+  getSettings: () => ipcRenderer.invoke('get-settings'),
+  saveSettings: (settings) => ipcRenderer.invoke('save-settings', settings),
 });
